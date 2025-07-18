799f01bd4c12bcad8fec770dc388b85a60546d69
Muhamad Faishal Azizi <62874580+faishalmhmd@users.noreply.github.com>
2025-07-18 14:05:54 +0700

    feat: oh pake char harus pake spasi

--- buffer.c
+++ buffer.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+
+int main () {
+    int angka;
+    char huruf;
+
+    printf("Masukkan Angka : ");
+    scanf("%d", &angka);
+
+    printf("Masukan Char: ");
+    scanf("%c", &huruf);
+
+    return 0;
+}
