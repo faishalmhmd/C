59177cf0840f5e2b7bb0781993dddb02380ea589
Muhamad Faishal Azizi <62874580+faishalmhmd@users.noreply.github.com>
2025-07-23 11:03:00 +0700

    masih bingung pointer

--- main.c
+++ main.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 int main() {
+    int var_a = 100;
+    int var_b = 200;
+    int *p_var_a = &var_a;
 
-    int array[5] = {1,2,3,4,5};
 
-    for (int i = 0; i < 5; i++) {
-        printf("element ke %d adalah %d \n",i,array[i]);
-    }
-
-    return 0;
+    printf("Name \t Addres \t \t Data \n");
+    printf("var_a\t %p \t %d \n",&var_a, var_a);
+    printf("var_b\t %p \t %d \n",&var_b, var_b);
+    printf("p_var_a\t %p \t %d \n",&p_var_a, *p_var_a);
 }
-
--- pointer.c
+++ pointer.c
@@ -1,13 +1,5 @@
 #include <stdio.h>
 
 int main() {
-    int arr[3] = {10, 20, 30};
-    int *p = arr;
-
-    for(int i=0; i<3; i++) {
-        printf("arr[%d] = %d, alamat = %p\n", i, *(p+i), (void*)(p+i));
-    }
-
-    return 0;
+    puts("helloWorld");
 }
-    
