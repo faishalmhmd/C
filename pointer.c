fe333937deb00103954ddf8e1f2b5d718f90d0d5
Muhamad Faishal Azizi <62874580+faishalmhmd@users.noreply.github.com>
2025-07-24 16:41:30 +0700

    aslkdjhawleha

--- pointer.c
+++ pointer.c
@@ -1,24 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main() {
-	struct Product {
-		char *name;
-		unsigned int price;
-		unsigned int stock;
-		float weight;
-	};
-
-	struct Product *buku = calloc(2, sizeof(struct Product));
-
-	buku->name = "Pemrograman C untuk pemula";
-	buku->price = 900000;
-	buku->stock = 5;
-	buku->weight = 1.2;
-
-	printf("## DETAIL PRODUK ## \n");
-	printf("name : %s \n", buku->name);
-	printf("harga : %d \n", buku->price);
-	printf("stok : %d \n", buku->stock);
-	printf("berat: %.2f kg \n", buku->weight);
+	int  a = 10;
+	int *pointerToA  = &a;
+	printf("var pointerToA = %d",*pointerToA);
 }
