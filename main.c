5ba30d43d5d184ba4bbae2f33be24d54fee4193e
Muhamad Faishal Azizi <62874580+faishalmhmd@users.noreply.github.com>
2025-07-24 10:11:22 +0700

    menghadeh

--- main.c
+++ main.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
-int main() {
-    int var_a = 100;
-    int var_b = 200;
-    int *p_var_a = &var_a;
+void add_score(int *score){
+    *score = *score + 5;
+    printf("score diubah ke: %d\n", *score);
+}
 
+void main(){
+    int score = 0;
 
-    printf("Name \t Addres \t \t Data \n");
-    printf("var_a\t %p \t %d \n",&var_a, var_a);
-    printf("var_b\t %p \t %d \n",&var_b, var_b);
-    printf("p_var_a\t %p \t %d \n",&p_var_a, *p_var_a);
+    printf("score sebelum diubah: %d\n", score);
+    add_score(&score);
+    printf("score setelah diubah: %d\n", score);
 }
--- pointer.c
+++ pointer.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-    puts("helloWorld");
+	struct Product {
+		char *name;
+		unsigned int price;
+		unsigned int stock;
+		float weight;
+	};
+
+	struct Product *buku = calloc(2, sizeof(struct Product));
+
+	buku->name = "Pemrograman C untuk pemula";
+	buku->price = 900000;
+	buku->stock = 5;
+	buku->weight = 1.2;
+
+	printf("## DETAIL PRODUK ## \n");
+	printf("name : %s \n", buku->name);
+	printf("harga : %d \n", buku->price);
+	printf("stok : %d \n", buku->stock);
+	printf("berat: %.2f kg \n", buku->weight);
 }
